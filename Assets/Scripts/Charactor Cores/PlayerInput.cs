eb26a8eb2ddcddc92811eacad46683cfa5be5e3e
Daejang <Kalby@dhcp-v022-167.mobile.uci.edu>
2016-10-19 21:42:52 -0700

    Make a file tree and a small input script
    input script for testing movement and other actions

---
+++ Assets/Scripts/Charactor Cores/PlayerInput.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+	public float xMove { get; private set; }
+	public float yMove { get; private set; }
+	public bool  jump  { get; private set; }
+	public bool  whack { get; private set; }
+
+
+	/*player movement script */
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		UpdateInput();
+		AssignInputToAction();
+	
+	}
+
+	void UpdateInput () {
+
+		xMove = Input.GetAxis("horizontal");
+		yMove = Input.GetAxis("vertical");
+		jump  = Input.GetKeyDown(KeyCode.Space);
+		whack = Input.GetKeyDown(KeyCode.J);
+
+
+		
+	}
+
+	void AssignInputToAction () {
+		
+	}
+}
