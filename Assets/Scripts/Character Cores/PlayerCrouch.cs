3a49de7fcdb765e5113e500b0248fecdd532246d
ksantaro <ubkenneths@gmail.com>
2016-10-26 21:40:38 -0700

    Working on Crouch

---
+++ Assets/Scripts/Character Cores/PlayerCrouch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCrouch : MonoBehaviour {
+
+	public int height; //public for unity
+
+
+	public void Crouch() {
+		this.height = this.height / 2;
+	}
+
+	public void Stand() {
+		this.height = this.height * 2;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		height = 6;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
