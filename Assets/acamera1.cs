f0a448812fc4fe407b19317829649e01e78e3531
TrevT17 <torrestr@uci.edu>
2016-10-26 21:22:05 -0800

    Camera follows player
    The camera will be focused on the player and will continue to follow the
    player even when the player moves.

---
+++ Assets/acamera1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+    private Vector2 velocity;
+
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    public GameObject player;
+
+    void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+	
+	}
+	
+	void FixedUpdate ()
+    {
+
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+
+    }
+
+}
